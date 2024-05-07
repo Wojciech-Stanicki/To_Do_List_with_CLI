7f45134d2e2c460ee070e8f48d8e9e4964914008
Wojciech-Stanicki <stanickiw@gmail.com>
2024-05-07 20:21:02 +0200

    add loadCsvData method that reads .csv file and saves data into 2D array

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,53 @@
 package pl.coderslab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
+
+
+    public static String[][] loadCsvData (String pathname) {
+
+        File tasksFile = new File(pathname);
+        String line;
+        String[] taskLines = new String[0];
+
+        try (Scanner lineScanner = new Scanner(tasksFile)) {
+            while (lineScanner.hasNextLine()) {
+                line = lineScanner.nextLine();
+                taskLines = appendArray(taskLines, line);
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("Couldn't find file");
+        }
+
+        String[][] tasksTable = new String[taskLines.length][3];
+        String[] taskElements;
+
+        for (int i = 0; i < taskLines.length; i++) {
+            taskElements = taskLines[i].split(",");
+            tasksTable[i] = taskElements;
+        }
+
+        return tasksTable;
+    }
+
+
+    public static String[] appendArray(String[] arrayToAppend, String appendment) {
+
+        String[] resultArray = Arrays.copyOf(arrayToAppend,arrayToAppend.length + 1);
+        resultArray[resultArray.length - 1] = appendment;
+
+        return resultArray;
+    }
+
 }
